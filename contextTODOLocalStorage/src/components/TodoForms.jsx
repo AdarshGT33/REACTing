364ea15805ed97a6b4313db5a86807344fb9ffc7
Adarsh <tomaradarsh18@gmail.com>
2024-07-27 10:29:35 +0530

    todoForm commit

--- contextTODOLocalStorage/src/components/TodoForms.jsx
+++ contextTODOLocalStorage/src/components/TodoForms.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {useTodo }from "../contexts/TodoContext"
+import { useTodo }from "../contexts/TodoContext"
 
 function TodoForms() {
     const [todo, setTodo] = useState("")
